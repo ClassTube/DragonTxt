d238c451f863d7812e559f163df02c526e3e93b3
kd2929 <110597375+kd2929@users.noreply.github.com>
2023-04-01 11:14:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6232350725:AAFFT1PF5eKJYgTtuHt_DPFrM_u9kJflals")
     API_ID = int(os.environ.get("API_ID", "10577960"))
     API_HASH = os.environ.get("API_HASH", "80fd047285f4e94ca80311928b6bb5da")
-    AUTH_USERS = "435946586"
+    
 
